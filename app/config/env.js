33db42271b5d9dff1c78ed5a0a6d958a74f4caea
fernando2575 <fernandodavid2575@gmail.com>
2024-11-04 12:21:17 -0600

    first commit

---
+++ app/config/env.js
@@ -0,0 +1,15 @@
+const env = {
+    database: 'umg_antigua2024_4521_ugmy',
+    username: 'umg_antigua2024_4521_user',
+    password: '4uoKaCbkdGUshs9FYmv6U5TK9YVY1fjY',
+    host: 'dpg-csk1a91u0jms73b648d0-a.oregon-postgres.render.com',
+    dialect: 'postgres',
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
+  
+  module.exports = env;
